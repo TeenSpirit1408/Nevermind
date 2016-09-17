3967535c3417abc88bbb07e34279f0b8b903b9cf
TeenSpirit1408 <Mentalist26021999@gmail.com>
2016-09-17 23:48:54 +0400

    Add files via upload

---
+++ App1.py
@@ -0,0 +1,15 @@
+import math
+import pylab
+from matplotlib import mlab
+tmin = -20.0
+tmax = 20.0
+dx = 0.01
+tlist = mlab.frange(tmin, tmax, dx)
+pylab.ion()
+for a in range(50):
+    ylist = [math.cos(2*t) for t in tlist]
+    xlist = [math.cos(t + a) for t in tlist]
+    pylab.clf()
+    pylab.plot(xlist, ylist)
+    pylab.draw()
+    pylab.pause(0.3)
